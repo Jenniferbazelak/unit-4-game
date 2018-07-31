2ea897771b3fb45820112395ff5119166967d5e9
Baz <jennigoldstein@gmail.com>
2018-07-30 21:08:02 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# unit-4-game
+# Crystal Collector
+
+##What is this?
+This is a game where you collect crystals.
+
+##How do you play?
+You will be given a random number at the start of the game.
+
+There are four crystals below. By clicking on a crystal, you will add a specific amount of points
+to your total score.
+
+You win the game by matching your total score to random number and you lose the game if your total score
+goes above the random number.
+
+The value of each crystal is hidden from you until you click on it.
+
+Each time when the game starts, the game will change the value of each crystal.
+
+## Where do I start?
+You can play here: 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,54 @@
+$(document).ready(function () {
 
-// set variables
-var wins = 0;
-var losses = 0;
-var crystalValue = 0;
-var playerScore = 0;
-var crystalValue1 = 0;
-var crystalValue2 = 0;
-var crystalValue3 = 0;
-var crystalValue4 = 0;
+    // set variables
+    var wins = 0;
+    var losses = 0;
+    var playerScore = 0;
+    var crystalValue1 = 0;
+    var crystalValue2 = 0;
+    var crystalValue3 = 0;
+    var crystalValue4 = 0;
 
-function getRandomNumber(minNumber, maxNumber) {
-    return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-}
+    //define functions
+    function getRandomNumber(minNumber, maxNumber) {
+        return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+    }
 
-function initializeGame() {
+    function initializeGame() {
+        randomNumber = getRandomNumber(19, 120);
+        $("#current-random-number").text(randomNumber);
+        crystalValue1 = getRandomNumber(1, 12);
+        crystalValue2 = getRandomNumber(1, 12);
+        crystalValue3 = getRandomNumber(1, 12);
+        crystalValue4 = getRandomNumber(1, 12);
+        playerScore = 0;
+        $("#player-score").text(playerScore);
+        console.log("current number is " + randomNumber);
+        console.log("Crystal value 1 is " + crystalValue1);
+        console.log("Crystal value 2 is " + crystalValue2);
+        console.log("Crystal value 3 is " + crystalValue3);
+        console.log("Crystal value 4 is " + crystalValue4);
+        console.log("player score is " + playerScore);
+
+    }
+
+    function win() {
+        wins++;
+        $("#wins").text(wins);
+        alert("You Win!");
+        initializeGame();
+    }
+
+    function lose() {
+        losses++;
+        $("#losses").text(losses);
+        alert("You Lose!");
+        initializeGame();
+    }
+
+
+    //set-up at initial screen
+
     var randomNumber = getRandomNumber(19, 120);
     $("#current-random-number").text(randomNumber);
     var crystalValue1 = getRandomNumber(1, 12);
@@ -24,34 +59,92 @@ function initializeGame() {
     $("#crystal-3").text(crystalValue3);
     var crystalValue4 = getRandomNumber(1, 12);
     $("#crystal-4").text(crystalValue4);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
     console.log("current number is " + randomNumber);
     console.log("Crystal value 1 is " + crystalValue1);
     console.log("Crystal value 2 is " + crystalValue2);
     console.log("Crystal value 3 is " + crystalValue3);
     console.log("Crystal value 4 is " + crystalValue4);
-    var playerScore = 0;
-    $("#player-score").text(playerScore);
 
-}
 
-initializeGame()
 
-$(".crystal").on("click", function () {
-    var randomNumber = $("#current-random-number").val();
-    var playerScore = $("#player-score").val();
-    var crystalValue = $(this).val();
-    playerScore= (playerScore + crystalValue)
-    if (randomNumber > playerScore) {
-        $("#player-score").text(crystalValue + playerScore);
-    }
-    else if (randomNumber === playerScore) {
-        wins++;
-        $("#wins").text(wins);
-        initializeGame();
-    }
-    else if (randomNumber < playerScore) {
-        loses++;
-        $("losses").text(losses);
-        initializeGame();
-    }
-})
+// when a crystal is clicked
+
+    $("#crystal-1").on("click", function () {
+        playerScore = playerScore + crystalValue1;
+        $("#player-score").text(playerScore);
+        console.log("The player score is now " + playerScore)
+        if (randomNumber === playerScore) {
+            win();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+        else if (randomNumber < playerScore) {
+            lose();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+
+    });
+
+    $("#crystal-2").on("click", function () {
+        playerScore = playerScore + crystalValue2;
+        $("#player-score").text(playerScore);
+        console.log("The player score is now " + playerScore)
+        if (randomNumber === playerScore) {
+            win();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+        else if (randomNumber < playerScore) {
+            lose();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+
+    });
+
+    $("#crystal-3").on("click", function () {
+        playerScore = playerScore + crystalValue3;
+        $("#player-score").text(playerScore);
+        console.log("The player score is now " + playerScore)
+        if (randomNumber === playerScore) {
+            win();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+        else if (randomNumber < playerScore) {
+            lose();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+
+    });
+
+    $("#crystal-4").on("click", function () {
+        playerScore = playerScore + crystalValue4;
+        $("#player-score").text(playerScore);
+        console.log("The player score is now " + playerScore)
+        if (randomNumber === playerScore) {
+            win();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+        else if (randomNumber < playerScore) {
+            lose();
+            playerScore = 0;
+            $("#player-score").text(playerScore);
+            initializeGame();
+        }
+
+    });
+
+});
